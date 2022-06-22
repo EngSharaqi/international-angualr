0c47df58513d6172dedebda0b1ab7c95ad347f7a
EngSharaqi <muhammadsharaqi@gmail.com>
2022-06-22 21:32:37 +0200

    faq

--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -8,5 +8,35 @@ import { Component, OnInit } from '@angular/core';
 export class FaqComponent {
 
   constructor() { }
-
+   faqs = {
+    _1: {
+      question: 'Why Lorem ipsum dolor ?',
+      answer: 'Lorem ipsum dolor umet dskdf sdfjdkf bla bla bla Lorem ipsum dolor umet dskdf sdfjdkf bla bla bla Lorem ipsum dolor umet dskdf sdfjdkf bla bla bla Lorem ipsum dolor umet dskdf sdfjdkf bla bla bla Lorem ipsum dolor umet dskdf sdfjdkf bla bla bla Lorem ipsum dolor umet dskdf sdfjdkf bla bla bla Lorem ipsum dolor umet dskdf sdfjdkf bla bla bla'
+    },
+    _2: {
+      question: 'how lorem ipsum dolor ?',
+      answer: 'dskdf sdfjdkf bla bla bla Lorem ipsum dolor umet dskdf ssdfjdkf bla bla bla'
+    },
+    _3: {
+      question: 'when dolor ipsum umet dolor ipsum ?',
+      answer: 'Lorem ipsum dolor umet dskdf sdfjdkf  dolor umet dskdf sdfjdkf bla bla bla Lorem ipsum dolor umet dskdf sdfjdkf bla bla bla Lorem ipsum dolor umet dskdf sdfjdkf bla bla bla Lorem ipsum dolor umet dskdf sdfjdkf bla bla bla Lorem ipsum dolor umet dskdf sdfjdkf bla bla bla'
+    },
+    _4: {
+      question: 'Who is lorem ipusm dolor umit lorem dolor ?',
+      answer: 'Lorem ipsum dolor umet dskdf ipsum dolor umet dskdf sdfjdkf bla bla bla Lorem ipsum dolor umet dskdf sdfjdkf bla bla bla Lorem ipsum dolor umet dskdf sdfjdkf bla bla bla Lorem ipsum dolor umet dskdf sdfjdkf bla bla bla Lorem ipsum dolor umet dskdf sdfjdkf bla bla bla Lorem ipsum dolor umet dskdf sdfjdkf bla bla bla'
+    },
+    _5: {
+      question: 'Hello from the other side :)',
+      answer: 'Lorem ipsum dolor umet dskdf sdfjdkf bla bla bla'
+    },
+    _6: {
+      question: 'Bla bla bla',
+      answer: 'umet dskdf sdfjdkf bla bla bla Lorem ipsum dolor umet dskdf sdfjdkf bla bla bla Lorem ipsum dolor umet dskdf sdfjdkf bla bla bla Lorem ipsum dolor umet dskdf sdfjdkf bla bla bla Lorem ipsum dolor umet dskdf sdfjdkf bla bla bla Lorem ipsum dolor umet dskdf sdfjdkf bla bla bla'
+    },
+    _7: {
+      question: 'what should I add',
+      answer: 'Lorem ipsum dolor umet dskdf sdfjdkf bla bla bla Lorem ipsum dolor umet dskdf sdfjdkf bla bla bla Lorem ipsum dolor umet dskdf sdfjdkf bla bla bla Lorem ipsum dolor umet dskdf sdfjdkf bla bla bla Lorem ipsum dolor umet dskdf sdfjdkf bla bla bla Lorem ipsum dolor umet dskdf sdfjdkf bla bla bla Lorem ipsum dolor umet dskdf sdfjdkf bla bla bla'
+    },
+   }
+  
 }
