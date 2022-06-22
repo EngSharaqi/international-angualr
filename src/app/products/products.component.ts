2bc49c104760c3fb7c2aee23b798bdd23a3ebaf4
EngSharaqi <muhammadsharaqi@gmail.com>
2022-06-22 22:06:22 +0200

    add products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -5,11 +5,69 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  products = [
+    {
+      name: 'Product Name',
+      description: 'bla bla bla',
+      img: '../../assets/images/I.jpg'
+  },
+  {
+    name: 'Product Name',
+    description: 'bla bla bla',
+    img: '../../assets/images/II.jpg'
+},
+{
+  name: 'Product Name',
+  description: 'bla bla bla',
+  img: '../../assets/images/III.jpg'
+},
+{
+  name: 'Product Name',
+  description: 'bla bla bla',
+  img: '../../assets/images/IIII.jpg'
+},
+{
+  name: 'Product Name',
+  description: 'bla bla bla',
+  img: '../../assets/images/I.jpg'
+},
+{
+name: 'Product Name',
+description: 'bla bla bla',
+img: '../../assets/images/II.jpg'
+},
+{
+name: 'Product Name',
+description: 'bla bla bla',
+img: '../../assets/images/III.jpg'
+},
+{
+name: 'Product Name',
+description: 'bla bla bla',
+img: '../../assets/images/IIII.jpg'
+},
+{
+  name: 'Product Name',
+  description: 'bla bla bla',
+  img: '../../assets/images/I.jpg'
+},
+{
+name: 'Product Name',
+description: 'bla bla bla',
+img: '../../assets/images/II.jpg'
+},
+{
+name: 'Product Name',
+description: 'bla bla bla',
+img: '../../assets/images/III.jpg'
+},
+{
+name: 'Product Name',
+description: 'bla bla bla',
+img: '../../assets/images/IIII.jpg'
+},
+  ];
 }
